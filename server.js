f90d9c7158913c408bbe4538a18c3c5ae73f5e28
Sebastian Lieschke <sebastianlieschke@web.de>
2018-08-31 21:26:49 +0200

    serialized db insert to try to prevent race conditions

--- server.js
+++ server.js
@@ -126,23 +126,26 @@ app.get('/view_trips', (req, res) => {
     }
 });
 
-app.post('/create_trip', (req, res) => {
-    console.log(req.body);
-    console.log(`INSERT INTO trips (boat, latitude, longitude, departure, arrival) VALUES (${req.body.boat_id}, ${req.body.latitude}, ${req.body.longitude}, ${req.body.departure}, ${req.body.arrival})`);
-    db.run('INSERT INTO trips (boat, latitude, longitude, departure, arrival) VALUES (?, ?, ?, ?, ?);', req.body.boat_id, req.body.latitude, req.body.longitude, req.body.departure, req.body.arrival, (err, trip) => {
-        if (err) {
-            console.log(`Error when requesting /create_trip`);
-            return res.json(err.message);
-        }
-        if (trip) {
-            console.log(`Rows inserted ${trip}`);
-            console.log(`Successfully requested /create_trip`);
-            return res.json({ success: true, trip });
-        }
-        else {
-            console.log(`Successfully requested /create_trip but didn't work`);
-            return res.json({ success: false, message: `trip couldnt't be created` });
-        };
+db.serialize(() => {
+    app.post('/create_trip', function (req, res) {
+        console.log(req.body);
+        console.log(`INSERT INTO trips (boat, latitude, longitude, departure, arrival) VALUES (${req.body.boat_id}, ${req.body.latitude}, ${req.body.longitude}, ${req.body.departure}, ${req.body.arrival})`);
+        db.run('INSERT INTO trips (boat, latitude, longitude, departure, arrival) VALUES (?, ?, ?, ?, ?);', req.body.boat_id, req.body.latitude, req.body.longitude, req.body.departure, req.body.arrival, (err, trip) => {
+            if (err) {
+                console.log(`Error when requesting /create_trip`);
+                return res.json(err.message);
+            }
+            if (trip) {
+                console.log(`Rows inserted ${trip}`);
+                console.log(`Successfully requested /create_trip`);
+                return res.json({ success: true, trip });
+            }
+            else {
+                console.log(trip);
+                console.log(`Successfully requested /create_trip but didn't work`);
+                return res.json({ success: false, message: `trip couldnt't be created` });
+            };
+        });
     });
 });
 
